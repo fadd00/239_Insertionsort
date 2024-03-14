bd8c7c2d0fa5db2256585a7bff9d0fe2a3cf722a
fadd00 <andhikahutama9@gmail.com>
2024-03-14 14:42:45 +0700

    buat insertionsort

--- smith.cpp
+++ smith.cpp
@@ -7,6 +7,13 @@ void InsrtionSort(){
 
     for(i = 1; i <= n-1; i++){ //step 1
         temp = arr[i]; //step 2
+
+        while (j >= 0 && arr[j] > temp) //step 4 
+        {
+            arr[j] = arr[j+1]; //step 4a
+            j--; //step 4b
+        }
+        
     }
 }
 
